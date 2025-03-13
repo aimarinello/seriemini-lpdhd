16a5afddc3ffd90d026703d8e7e19b841050d48b
Adolfo Marinello <aimarinello@uc.cl>
2025-03-13 16:03:46 -0300

    Cambio Header.svelte

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -6,7 +6,7 @@
   const backgroundImage = serieConfig.banner !== undefined ? (/(^http)/.test(serieConfig.banner) ? serieConfig.banner : `${base}/${serieConfig.banner}`) : undefined;
 </script>
 
-<header class="main-header" style={`background-image: ${ backgroundImage !== undefined ? `url(${backgroundImage})` : "none"};`}>
+<header class="main-header" style={`background-image: ${ backgroundImage !== undefined ? `url(${backgroundImage})` : "none"};background-color: var(--color, #cda);`}>
   <div>
     <h1 class="main-title" id="main-title"><a href={base}>{serieConfig.title}</a></h1>
     <span class="main-subtitle">{serieConfig.subtitle}</span>
