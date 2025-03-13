75d36e395aad0eb2d17f168ad958c1d655884cbb
Adolfo Marinello <aimarinello@uc.cl>
2025-03-13 16:33:40 -0300

    Prueba color 2

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -8,7 +8,7 @@
 
 <header class="main-header" style={`background-image: ${ backgroundImage !== undefined ? `url(${backgroundImage})` : "none"};background-color: var(--color, #f3a00f);`}>
   <div>
-    <h1 class="main-title"  style="color:red" id="main-title"><a href={base}>{serieConfig.title}</a></h1>
+    <h1 class="main-title"  style="color:red;" id="main-title"><a href={base}>{serieConfig.title}</a></h1>
     <span class="main-subtitle">{serieConfig.subtitle}</span>
   </div>
   
